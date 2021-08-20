6e530fbce3cf6519f6d5ddba8737c601a9dec2ae
twipz333 <53153082+twipz333@users.noreply.github.com>
2021-08-20 16:19:27 +0300

    Initial commit

---
+++ flask_server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+import main
+app = Flask('Map', static_url_path='/static')
+
+@app.route('/')
+def index():
+    main.getmap()
+    return app.send_static_file('map.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,16 @@
+import os, ipyleaflet
+from ipywidgets import Layout
+
+defaultLayout = Layout(width='100%', height='800px') #Layout карты (для правильного отображения)
+
+Map = ipyleaflet.Map(zoom=6, layout=defaultLayout) #Создание карты
+
+os.chdir(os.path.dirname(os.path.realpath(__file__))) #Установка рабочей директории
+
+def getmap(name: str = 'map') -> str:
+    html_file = os.path.join(os.getcwd(), 'static', f'{name}.html')
+    Map.save(html_file)
+    return name
+
+if __name__ == '__main__':
+    getmap()
